f68cda0f42cfb4b172630a2056675fe8add329a8
kingyaaa <59854231+kingyaaa@users.noreply.github.com>
2020-02-13 16:46:30 +0800

    Update admin.h

--- my type/admin.h
+++ my type/admin.h
@@ -1,18 +1,23 @@
 #pragma once
 #include"Goods.h"
 #include <iostream>
+#include<string>
+#include<algorithm>
+#include<iomanip>
 #define ADMIN_ID_MAX 100
 #define ADMIN_PASSWORD_MAX 100
 using namespace std;
 class Admin
 {
-	Goods g;
 private:
 	char* id;
 	char* password;
+	Goods a;
 public:
 	Admin();
 	Admin(char* cid, char* cpassword);
+	void Opetion();
+	void Title();
 	void Login();
 	void Logout();
 	void InquireGoods();
@@ -20,5 +25,8 @@ public:
 	void DeleteGoods();
 	void ModifyGoodsNum();
 	void InquireSaleList();
+	void HotList();
+	void UnpopList();
+	void SupplyAlarm();
 	~Admin();
 };
